47a34d24e352734c5f28f6a8736086801f4d7dcf
Morthang <wollymati@gmail.com>
2022-07-26 22:00:38 +0200

    Creating the RankedGradeBook class

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using GradeBook.Enums;
+
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            var studentsTwentyPercent = Math.Ceiling(0.2 * Students.Count);
+            var higherAverageGradeStudents = 0;
+
+            foreach (var student in Students)
+            {
+
+                if (student.AverageGrade > averageGrade)
+                {
+                    higherAverageGradeStudents++;
+                }
+
+            }
+            var downGrade = (int)Math.Floor(higherAverageGradeStudents / studentsTwentyPercent);
+            var gradeToLetter = new List<char> { 'A', 'B','C', 'D', 'E', 'F' };
+            return gradeToLetter[downGrade];
+        }
+
+
+    }
+}
